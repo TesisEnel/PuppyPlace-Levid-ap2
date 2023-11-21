6e2fbad95b80c443abf4273fdd3462e6c1b936c0
Greypolanco <greilyntejada03@gmail.com>
2023-11-21 19:17:54 -0400

    creando LoadingScreen y agregando imagen del logo

---
+++ app/src/main/java/com/project/puppyplace/PuppyPlace.kt
@@ -0,0 +1,9 @@
+package com.project.puppyplace
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+@HiltAndroidApp
+class PuppyPlace : Application() {
+}
--- app/src/main/java/com/project/puppyplace/ui/theme/Color.kt
+++ app/src/main/java/com/project/puppyplace/ui/theme/Color.kt
@@ -1,11 +1,11 @@
 package com.project.puppyplace.ui.theme
 
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+/*Modo Light*/
+private val LightColorScheme = lightColorScheme(
+    primary = Color(0xFEB72B),
+    secondary = Color(0xFEB72B),
+    tertiary = Color(0xFEB72B)
+)
