8e8355841f41b047a7153526de3871497ec98e56
Greypolanco <greilyntejada03@gmail.com>
2023-11-21 22:58:01 -0400

    Especificacion de los colores, tema: Anek latin y modficiacion del background

--- app/src/main/java/com/project/puppyplace/ui/theme/Color.kt
+++ app/src/main/java/com/project/puppyplace/ui/theme/Color.kt
@@ -4,8 +4,12 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.ui.graphics.Color
 
 /*Modo Light*/
-private val LightColorScheme = lightColorScheme(
-    primary = Color(0xFEB72B),
-    secondary = Color(0xFEB72B),
-    tertiary = Color(0xFEB72B)
-)
+val Yellow25 = Color(0xFF4E2A00)
+val Yellow40 = Color(0xFFFEB72B)
+val Yellow50 = Color(0xFFF6B94D)
+val Yellow60 = Color(0xFFE7BE69)
+val Yellow100 = Color(0xFFFFFFFF)
+
+
+
+val GreenYellow90 = Color(0xFFF1F1D6)
