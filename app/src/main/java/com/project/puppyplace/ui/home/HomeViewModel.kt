f9a0205d3db0b5e4a0dfde6908c208de185e6fa6
SirLevid <jassontejada@gmail.com>
2023-11-22 14:27:10 -0400

    Creating Home

---
+++ app/src/main/java/com/project/puppyplace/ui/home/HomeViewModel.kt
@@ -0,0 +1,38 @@
+package com.project.puppyplace.ui.home
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.project.puppyplace.data.repository.HomeRepository
+import com.project.puppyplace.util.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+    private val homeRepository: HomeRepository
+): ViewModel(){
+    private var _state = MutableStateFlow(HomeListState())
+    val state: StateFlow<HomeListState> = _state.asStateFlow()
+    init {
+        homeRepository.getDogs().onEach { result ->
+            when (result) {
+                is Resource.Loading -> {
+                    _state.value = HomeListState(isLoading = true)
+                }
+
+                is Resource.Success -> {
+                    _state.value = HomeListState(dogsList = result.data ?: emptyList())
+                }
+
+                is Resource.Error -> {
+                    _state.value = HomeListState(error = result.message ?: "Unknown error")
+                }
+            }
+        }.launchIn(viewModelScope)
+    }
+}
