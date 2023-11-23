7dd56f88bd621e8202ca1784b4fcce8c3a49c7be
SirLevid <jassontejada@gmail.com>
2023-11-22 22:32:07 -0400

    Changing fieldNames

--- app/src/main/java/com/project/puppyplace/data/remote/dto/UserDto.kt
+++ app/src/main/java/com/project/puppyplace/data/remote/dto/UserDto.kt
@@ -3,30 +3,19 @@ package com.project.puppyplace.data.remote.dto
 import com.squareup.moshi.Json
 
 data class UserDto(
-    @Json(name = "UserId")
+    @Json(name = "userId")
     val id: Int,
-    @Json(name = "Name")
     val name: String,
-    @Json(name = "Surname")
     val surname: String,
-    @Json(name = "IdentificationNumber")
     val identificationNumber: String,
-    @Json(name = "Address")
     val address: String,
-    @Json(name = "Email")
     val email: String,
-    @Json(name = "Password")
     val password: String,
-    @Json(name = "Telephone")
     val telephone: String,
-    @Json(name = "Cellphone")
     val cellphone: String,
-    @Json(name = "Sex")
+    @Json(name = "sex")
     val gender: String,
-    @Json(name = "AppointmentDate")
     val appointmentDate: String? = null,
-    @Json(name = "HouseType")
     val houseType: String,
-    @Json(name = "Role")
     val role: String = "User"
 )
