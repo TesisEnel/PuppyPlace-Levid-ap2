6ec6ae36d75d6ad69e884d7fe771e140c3d02d0b
SirLevid <jassontejada@gmail.com>
2023-11-24 01:53:44 -0400

    Search filter by breed

--- app/src/main/java/com/project/puppyplace/ui/home/HomeScreen.kt
+++ app/src/main/java/com/project/puppyplace/ui/home/HomeScreen.kt
@@ -15,6 +15,8 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.HeartBroken
@@ -36,6 +38,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -61,8 +65,8 @@ fun HomeScreenContent(
 ){
     Column {
         HomeTopBar()
-        SearchTextField()
-        ChipGroup()
+        SearchTextField(viewModel)
+        ChipGroup(viewModel)
         DogsList(dogsList = dogsList, navController = navController, viewModel = viewModel)
     }
 }
@@ -95,16 +99,35 @@ fun HomeTopBar(){
     )
 }
 @Composable
-fun SearchTextField(){
+fun SearchTextField(viewModel: HomeViewModel){
     TextField(
-        value = "",
-        onValueChange = { /*TODO*/ },
-        placeholder = { Text(text = "Search")},
-        trailingIcon = {
-            Icon(
-                imageVector = Icons.Filled.Search,
-                contentDescription = "Search"
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Text,
+            imeAction = ImeAction.Search
+        ),
+        keyboardActions = KeyboardActions(
+            onSearch = {
+                viewModel.getDogsByBreed(viewModel.searchItem)
+            }
+        ),
+        maxLines = 1,
+        value = viewModel.searchItem,
+        onValueChange = { viewModel.onSearchItemChanged(it) },
+        label = { Text(text = "Search") },
+        placeholder = {
+            Text(
+                text = "Golden",
+                style = MaterialTheme.typography.labelSmall,
+                color = MaterialTheme.colorScheme.outline
             )
+        },
+        trailingIcon = {
+            IconButton(onClick = { viewModel.getDogsByBreed(viewModel.searchItem) }) {
+                Icon(
+                    imageVector = Icons.Filled.Search,
+                    contentDescription = "Search"
+                )
+            }
         },
         modifier = Modifier
             .padding(horizontal = 16.dp)
@@ -118,7 +141,7 @@ fun SearchTextField(){
 }
 
 @Composable
-fun ChipGroup(){
+fun ChipGroup(viewModel: HomeViewModel){
     Column(
         modifier = Modifier
             .fillMaxWidth()
@@ -128,20 +151,23 @@ fun ChipGroup(){
         Row(
             modifier = Modifier.horizontalScroll(rememberScrollState())
         ) {
-            Column {
-                Text(
-                    text = "More",
-                    modifier = Modifier
-                        .padding(8.dp),
-                    color = MaterialTheme.colorScheme.onTertiary,
-                    style = MaterialTheme.typography.titleLarge
+            Column(
+                modifier = Modifier.padding(end = 8.dp)
+            ){
+                AssistChip(
+                    onClick = { viewModel.getDogs() },
+                    label = { Text(text = "All") },
+                    colors = AssistChipDefaults.assistChipColors(
+                        containerColor = MaterialTheme.colorScheme.primary,
+                        labelColor = MaterialTheme.colorScheme.onTertiary
+                    )
                 )
             }
             Column(
                 modifier = Modifier.padding(end = 8.dp)
             ){
                 AssistChip(
-                    onClick = { /*TODO*/ },
+                    onClick = { viewModel.getDogsBySex("female") },
                     label = { Text(text = "Female") },
                     colors = AssistChipDefaults.assistChipColors(
                         containerColor = MaterialTheme.colorScheme.primary,
@@ -153,7 +179,7 @@ fun ChipGroup(){
                 modifier = Modifier.padding(end = 8.dp)
             ) {
                 AssistChip(
-                    onClick = { /*TODO*/ },
+                    onClick = { viewModel.getDogsBySex("male") },
                     label = { Text(text = "Male") },
                     colors = AssistChipDefaults.assistChipColors(
                         containerColor = MaterialTheme.colorScheme.onTertiary,
@@ -165,8 +191,8 @@ fun ChipGroup(){
                 modifier = Modifier.padding(end = 8.dp)
             ) {
                 AssistChip(
-                    onClick = { /*TODO*/ },
-                    label = { Text(text = "Corgi") },
+                    onClick = { viewModel.getDogsBySize("small") },
+                    label = { Text(text = "Small") },
                     colors = AssistChipDefaults.assistChipColors(
                         containerColor = MaterialTheme.colorScheme.secondary,
                         labelColor = MaterialTheme.colorScheme.onTertiary
@@ -177,8 +203,20 @@ fun ChipGroup(){
                 modifier = Modifier.padding(end = 8.dp)
             ){
                 AssistChip(
-                    onClick = { /*TODO*/ },
-                    label = { Text(text = "Golden") },
+                    onClick = { viewModel.getDogsBySize("medium") },
+                    label = { Text(text = "Medium") },
+                    colors = AssistChipDefaults.assistChipColors(
+                        containerColor = MaterialTheme.colorScheme.primary,
+                        labelColor = MaterialTheme.colorScheme.onTertiary
+                    )
+                )
+            }
+            Column(
+                modifier = Modifier.padding(end = 8.dp)
+            ){
+                AssistChip(
+                    onClick = { viewModel.getDogsBySize("big") },
+                    label = { Text(text = "Big") },
                     colors = AssistChipDefaults.assistChipColors(
                         containerColor = MaterialTheme.colorScheme.primary,
                         labelColor = MaterialTheme.colorScheme.onTertiary
@@ -243,7 +281,7 @@ fun DogItem(
                 Row {
                     Column {
                         Text(
-                            text = "40 years",
+                            text = "${dog.age} years",
                             modifier = Modifier
                                 .padding(horizontal = 8.dp, vertical = 4.dp),
                             color = MaterialTheme.colorScheme.onSecondary
