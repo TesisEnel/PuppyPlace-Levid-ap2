37eb9d5d07518ac1b99187139c42e33b1f85ce27
Greypolanco <greilyntejada03@gmail.com>
2023-11-26 17:11:14 -0400

    Finalizando Diseño de UserScreen

--- app/src/main/java/com/project/puppyplace/ui/user/UserScreen.kt
+++ app/src/main/java/com/project/puppyplace/ui/user/UserScreen.kt
@@ -13,14 +13,18 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material.icons.filled.Logout
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material.icons.filled.PhoneAndroid
+import androidx.compose.material.icons.filled.Update
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
@@ -43,6 +47,7 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
 import com.project.puppyplace.R
+import com.project.puppyplace.data.remote.dto.AppointmentDto
 
 @Composable
 fun UserScreen(
@@ -50,6 +55,7 @@ fun UserScreen(
     navController: NavController
 ){
     val user by viewModel.state.collectAsStateWithLifecycle()
+    val appointments by viewModel.stateAdoption.collectAsStateWithLifecycle()
     Box(
         modifier = Modifier.fillMaxSize()
     ) {
@@ -68,7 +74,7 @@ fun UserScreen(
             TopBar(navController = navController)
             NameUserText(viewModel = viewModel)
             InfoUser(viewModel = viewModel)
-            Appoiment()
+            AppointmentsList(appointments.adoptionList)
         }
     }
 }
@@ -105,7 +111,9 @@ fun InfoUser(viewModel: UserViewModel){
         colors = CardDefaults.cardColors(
             containerColor= MaterialTheme.colorScheme.secondary
         ),
-        border = CardDefaults.outlinedCardBorder(enabled = true),
+        border = CardDefaults.outlinedCardBorder(enabled = false),
+        modifier = Modifier
+            .fillMaxWidth()
     ){
         Row {
             Column(
@@ -115,7 +123,7 @@ fun InfoUser(viewModel: UserViewModel){
             ){
                 Row {
                     Icon(imageVector = Icons.Filled.Phone, contentDescription = "Telephone")
-                    Text(text = viewModel.user.telephone,)
+                    Text(text = viewModel.user.telephone)
                 }
             }
             Column(
@@ -207,9 +215,82 @@ fun TopBar(navController: NavController, viewModel: UserViewModel= hiltViewModel
 }
 
 @Composable
-fun Appoiment(){
+fun AppointmentsList(appoiments: List<AppointmentDto>){
     Text(text = "Appointments: ",
         style = MaterialTheme.typography.bodySmall
     )
+    LazyColumn{
+        items(appoiments){
+            appoiment -> AppoimentItem(appoiment)
+        }
+    }
 
+}
+
+@Composable
+fun AppoimentItem(appoiment: AppointmentDto){
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor= MaterialTheme.colorScheme.tertiary
+        ),
+        border = CardDefaults.outlinedCardBorder(enabled = false),
+        modifier = Modifier
+            .fillMaxWidth()
+    ){
+        Row {
+            Column(
+                modifier = Modifier
+                    .padding(8.dp)
+
+            ) {
+                Row {
+                    Icon(imageVector = Icons.Filled.Update, contentDescription = "Update")
+                    Text(text = appoiment.date)
+                    Text(text = "-" + appoiment.address)
+                }
+            }
+        }
+        Row {
+            Column(
+                modifier = Modifier
+                    .padding(8.dp)
+            ) {
+                Row {
+                    Icon(imageVector = Icons.Filled.Person, contentDescription = "person")
+                    Text(text = appoiment.userName)
+                    Text(text = appoiment.userSurname)
+                }
+            }
+            Column(
+                modifier = Modifier
+                    .padding(8.dp)
+            ) {
+                Row {
+                    Icon(imageVector = Icons.Filled.Email, contentDescription = "email")
+                    Text(text = appoiment.email)
+                }
+            }
+        }
+        Row {
+            Column(
+                modifier = Modifier
+                    .padding(8.dp)
+            ) {
+                Row {
+                    Icon(imageVector = Icons.Filled.Phone, contentDescription = "telephone")
+                    Text(text = appoiment.telephone)
+                }
+            }
+            Column(
+                modifier = Modifier
+                    .padding(8.dp)
+            ) {
+                Row {
+                    Icon(imageVector = Icons.Filled.PhoneAndroid, contentDescription = "cellphone")
+                    Text(text = appoiment.cellphone)
+                }
+            }
+        }
+    }
+    Spacer(modifier = Modifier.height(8.dp))
 }
