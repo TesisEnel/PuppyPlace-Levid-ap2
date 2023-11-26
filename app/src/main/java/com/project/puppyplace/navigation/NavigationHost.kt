26a7bb505681df7b1301caa35800aa1715af97de
Greypolanco <greilyntejada03@gmail.com>
2023-11-26 15:30:57 -0400

    Agregando navegacion a la pantalla user

--- app/src/main/java/com/project/puppyplace/navigation/NavigationHost.kt
+++ app/src/main/java/com/project/puppyplace/navigation/NavigationHost.kt
@@ -10,6 +10,7 @@ import com.project.puppyplace.ui.home.HomeScreen
 import com.project.puppyplace.ui.like.LikeScreen
 import com.project.puppyplace.ui.login.LoginScreen
 import com.project.puppyplace.ui.signup.SignUpScreen
+import com.project.puppyplace.ui.user.UserScreen
 
 @Composable
 fun NavigationHost() {
@@ -35,6 +36,9 @@ fun NavigationHost() {
         }
         composable(Destination.like.route){
             LikeScreen(navController = navController)
+        }
+        composable(Destination.user.route){
+            UserScreen(navController = navController)
         }
     }
 }
