2db72d0141f940734a0e958dd66851db83725bd4
SirLevid <jassontejada@gmail.com>
2023-11-25 21:36:32 -0400

    Creating AppointmentDto for appointments

---
+++ app/src/main/java/com/project/puppyplace/data/remote/dto/AppointmentDto.kt
@@ -0,0 +1,13 @@
+package com.project.puppyplace.data.remote.dto
+
+data class AppointmentDto(
+    val id: Int = 0,
+    val date: String = "",
+    val userName: String = "",
+    val userSurname: String = "",
+    val identificationNumber: String = "",
+    val dogId: Int = 0,
+    val telephone: String = "",
+    val cellphone: String = "",
+    val email: String = "",
+)
