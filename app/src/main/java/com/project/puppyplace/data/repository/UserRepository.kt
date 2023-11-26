7e627e8b0c78de469da9e09f3ccc813351c988bf
Greypolanco <greilyntejada03@gmail.com>
2023-11-26 15:28:40 -0400

    Agregando el getbyId de Usuario

---
+++ app/src/main/java/com/project/puppyplace/data/repository/UserRepository.kt
@@ -0,0 +1,31 @@
+package com.project.puppyplace.data.repository
+
+import com.project.puppyplace.data.remote.PuppyPlaceAPI
+import com.project.puppyplace.data.remote.dto.DogDto
+import com.project.puppyplace.data.remote.dto.UserDto
+import com.project.puppyplace.util.Resource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
+
+class UserRepository @Inject constructor(
+    private val puppyPlaceApi: PuppyPlaceAPI
+) {
+    fun getUsers(): Flow<Resource<List<UserDto>>> = flow {
+        try {
+            emit(Resource.Loading())
+
+            val users = puppyPlaceApi.getUsers()
+
+            emit(Resource.Success(users))
+        } catch (e: HttpException) {
+            emit(Resource.Error(e.message ?: "Error HTTP GENERAL"))
+        } catch (e: IOException) {
+            emit(Resource.Error(e.message ?: "verificar tu conexion a internet"))
+        }
+    }
+
+    suspend fun getUserById(id: Int) = puppyPlaceApi.getUsersById(id)
+}
